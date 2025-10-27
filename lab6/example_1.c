71945a322855082ce47fa7e11f0389b7ef67e4ad
Yewon Park <ypa53@sfu.ca>
2025-10-26 23:02:49 -0700

    lab9

---
+++ lab6/example_1.c
@@ -0,0 +1,87 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ASSERT(expr)                                                           \
+  {                                                                            \
+    if (!(expr)) {                                                             \
+      fprintf(stderr, "Assertion failed: %s\n", #expr);                        \
+      exit(1);                                                                 \
+    }                                                                          \
+  }
+
+#define TEST(expr)                                                             \
+  {                                                                            \
+    if (!(expr)) {                                                             \
+      fprintf(stderr, "Test failed: %s\n", #expr);                             \
+      exit(1);                                                                 \
+    } else {                                                                   \
+      printf("Test passed: %s\n", #expr);                                      \
+    }                                                                          \
+  }
+
+typedef struct node {
+  uint64_t data;
+  struct node *next;
+} node_t;
+
+node_t *head = NULL;
+
+void insert_sorted(uint64_t data) {
+  node_t *new_node = malloc(sizeof(node_t));
+  new_node->data = data;
+  new_node->next = NULL;
+
+  if (head == NULL) {
+    head = new_node;
+    return;
+  }
+
+  node_t *curr = head;
+  node_t *prev = NULL;
+
+  while (curr != NULL && curr->data < data) {
+    prev = curr;
+    curr = curr->next;
+  }
+
+  if (prev == NULL) {
+    new_node->next = head;
+    head = new_node;
+  } else {
+    prev->next = new_node;
+    new_node->next = curr;
+  }
+}
+
+int index_of(uint64_t data) {
+  node_t *curr = head;
+  int index = 0;
+
+  while (curr != NULL) {
+    if (curr->data == data) {
+      return index;
+    }
+    curr = curr->next;
+    index++;
+  }
+
+  return -1;
+}
+
+int main() {
+  insert_sorted(1);
+  insert_sorted(2);
+  insert_sorted(5);
+  insert_sorted(3);
+
+  TEST(index_of(3) == 2);
+
+  insert_sorted(0);
+  insert_sorted(4);
+
+  TEST(index_of(4) == 4);
+
+  return 0;
+}
